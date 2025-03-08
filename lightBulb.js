a67c4a055415d3aecc282ac9566a8521690bfd60
MishkatHossainMilon <mishkat.hossain.milon@gmail.com>
2025-03-08 17:05:24 +0600

    css and js file complete

---
+++ lightBulb.js
@@ -0,0 +1,8 @@
+const switchBtn = document.querySelector('.switch');
+console.log(switchBtn);
+const body = document.body;
+
+switchBtn.addEventListener("click", ()=>{
+  // body.classList.toggle("on");
+  body.classList.toggle("on");
+})
